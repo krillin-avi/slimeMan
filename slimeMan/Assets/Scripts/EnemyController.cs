57e67ee1c0b351d917328f1de0b14d491ddd0154
krillin-avi <cesdaille@knights.ucf.edu>
2023-02-03 19:51:05 -0500

    Add AI to both Maps

---
+++ slimeMan/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    UnityEngine.AI.NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        
+    }
+}
